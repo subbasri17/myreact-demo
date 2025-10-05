daf5a189ba4811fc1e581d181e51fe8aab4648bd
Prakash-Devops <74908064+Prakash1810@users.noreply.github.com>
2025-10-05 11:51:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Guvi Tutor Demo!</h1>
+      <p>This page is generated using React.</p>
+    </div>
+  );
+}
+
+export default App;
